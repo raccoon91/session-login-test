f8a0073b640724c92e3eb8f77a77f91b147bfd7f
raccoon91 <qsx314@gmail.com>
2019-09-28 14:24:09 +0900

    add crypto

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const crypto = require("crypto");
 const models = require("../models");
 
 router.get("/sign_up", function(req, res, next) {
   res.render("user/signup");
 });
 
-router.post("/sign_up", function(req, res, next) {
+router.post("/sign_up", async function(req, res, next) {
   let body = req.body;
 
-  models.user
-    .create({
-      name: body.userName,
-      email: body.userEmail,
-      password: body.password
-    })
-    .then(result => {
-      res.redirect("/users/sign_up");
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  let inputPassword = body.password;
+  let salt = Math.round(new Date().valueOf() * Math.random()) + "";
+  let hashPassword = crypto
+    .createHash("sha512")
+    .update(inputPassword + salt)
+    .digest("hex");
+
+  let result = models.user.create({
+    name: body.userName,
+    email: body.userEmail,
+    password: hashPassword,
+    salt: salt
+  });
+
+  res.redirect("/users/sign_up");
 });
 
 module.exports = router;
