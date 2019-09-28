5a2a177e71ac8ef6f97ad2aeef5ff8c887c37494
raccoon91 <qsx314@gmail.com>
2019-09-28 14:33:15 +0900

    login page

--- routes/users.js
+++ routes/users.js
@@ -27,4 +27,41 @@ router.post("/sign_up", async function(req, res, next) {
   res.redirect("/users/sign_up");
 });
 
+// 메인 페이지
+router.get("/", function(req, res, next) {
+  res.send("환영합니다~");
+});
+
+// 로그인 GET
+router.get("/login", function(req, res, next) {
+  res.render("user/login");
+});
+
+// 로그인 POST
+router.post("/login", async function(req, res, next) {
+  let body = req.body;
+
+  let result = await models.user.findOne({
+    where: {
+      email: body.userEmail
+    }
+  });
+
+  let dbPassword = result.dataValues.password;
+  let inputPassword = body.password;
+  let salt = result.dataValues.salt;
+  let hashPassword = crypto
+    .createHash("sha512")
+    .update(inputPassword + salt)
+    .digest("hex");
+
+  if (dbPassword === hashPassword) {
+    console.log("비밀번호 일치");
+    res.redirect("/users");
+  } else {
+    console.log("비밀번호 불일치");
+    res.redirect("/users/login");
+  }
+});
+
 module.exports = router;
