57687e85b2788738fd33b507dd3b69350bb6d52c
raccoon91 <qsx314@gmail.com>
2019-09-28 14:09:32 +0900

    add sign up page

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+
+router.get("/sign_up", function(req, res, next) {
+  res.render("user/signup");
+});
+
+router.post("/sign_up", function(req, res, next) {
+  let body = req.body;
+
+  models.user
+    .create({
+      name: body.userName,
+      email: body.userEmail,
+      password: body.password
+    })
+    .then(result => {
+      res.redirect("/users/sign_up");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
